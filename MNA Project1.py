5b4e0b9ea3868585529e66ad26d15c561923059a
MNA2025 <muhammednur@gmail.com>
2025-02-14 23:10:18 +0300

    Son çalışmalar yaptığım taslak bu döküman

--- MNA Project1.py
+++ MNA Project1.py
@@ -1,2 +1,39 @@
-print("hello, world!")
-"Not: Sevgili Tekin hocam, Github ve Git senkronizasyonlarını tamamladım. Ama sonuçlarını göremiyorum."
+print("hello world!")
+# print("hello world")
+
+"""
+Variables (Değişkenler)
+Çoklu satır
+
+#string (katar) = iki tırnak işareti içine yazılan herşey
+
+''
+""
+"''"
+""""""
+"""
+
+a = 9
+b = 11
+toplam = a + b
+print(toplam)
+
+first_string = "Muhammed Nur"
+second_string = "ece"
+third_string = "meltem"
+
+strl = """üç tırnak ile başlayıp "bazı" kelimeleri çift tırnak içine almak ve 'bazı' tek tırnaklar kullanmak mümkün"""
+print(strl)
+
+# strl = 'MNA çalışma notu. Python öğreniminde "w3sochool.com" sitesinden alıştırmalar yapmam gerek.'
+
+print("Toplam = toplam")
+
+print("Toplam = 8")
+
+print('{0} {1} {2}' .format("toplam", "=", toplam))
+print('{0} {1} {2}' .format("toplam", "=", a + b))
+
+
+
+
